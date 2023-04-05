4e3974f6e358a5298a29c981884b0e68869c687a
Gavin-1001 <gavinshelley10@gmail.com>
2023-04-05 09:22:42 +0100

    Removed the Router from App

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
 import Navbar from "./Navbar"
 import Home from "./pages/Home"
 import About from "./pages/About"
-import { Route, Routes, createBrowserRouter, } from "react-router-dom"
+import { Route, Routes, BrowserRouter, } from "react-router-dom"
 import Login from "./pages/Login"
 
 
 
-const router = createBrowserRouter([
-  {path:'/', element: <Login />},
-  {path:'/about', element: <About />}
-]);
+// const router = createBrowserRouter([
+//   {path:'/', element: <Login />},
+//   {path:'/about', element: <About />}
+// ]);
 
 
 
 function App() {
-  // return (
-  //   // <>
-  //   //   <Navbar />
-  //   //   <div className="container">
-  //   //     <Routes>
-  //   //       <Route path="/" element={<Home />} />
-  //   //       <Route path="/login" element={<Login />} />
-  //   //       <Route path="/about" element={<About />} />
-  //   //     </Routes>
-  //   //   </div>
-  //   // </>
-  // )
+  return (
+    <>
+      <Navbar />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </>
+  )
 
-  return <RouterProvider router={router} />;
+  //return <RouterProvider router={router} />;
 }
 
 export default App
